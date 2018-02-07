fe977bc2f2d35045930751b3bf7961ef3ebce006
Ravi Ganta <ravi.ganta.2016@outlook.com>
2018-02-06 21:47:23 -0600

    Initial code commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LESumDiffWordProblem.printProblemsWithMultipleChoiceAnswers(5);
+		LEYoungerOlderWordProblem.printProblemsWithMultipleChoiceAnswers(5);
+		LENickelDimeProblem.printProblemsWithMultipleChoiceAnswers(5);
+		LESalineSolutionWordProblem.printProblemsWithMultipleChoiceAnswers(5);
+		LEUpstreamDownstreamProblem.printProblemsWithMultipleChoiceAnswers(5);
+		
+		//LESequenceProblem.printProblemsWithMultipleChoiceAnswers(12);
+		
+		LEYoungerOlderWordProblem.printProblemsWithMultipleChoiceAnswers(6);
+		
+		//PolynomialDivisionProblem.printProblemsWithMultipleChoiceAnswers(25);
+		//PolynomialFactorizationProblem.printProblemsWithMultipleChoiceAnswers(10);
+		
+		/*ComplexNumber c = ComplexNumber.getRandomComplexNumber();
+		System.out.println(c.toString());
+		
+		ComplexNumber simpleC = ComplexNumber.simplify(c);
+		System.out.println(simpleC.toString());*/
+	}
+
+}
